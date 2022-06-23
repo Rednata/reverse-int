59c052aacdf5dd30f78bf7bd89deed49b56f463d
Rednata <maknatosh@gmail.com>
2022-06-23 10:02:31 +0700

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+
+  n = String(Math.abs(n));
+  let newNumber = '';
+  for (let i = n.length-1; i >= 0; i--) {
+    newNumber = newNumber + n[i];
+  }
+  return newNumber;
 }
